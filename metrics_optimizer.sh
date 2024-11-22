9eb566e05e14146f1e6fc8e10701243a87e9b2bd
Andrii Koktash <102880571+AndriiKok@users.noreply.github.com>
2024-11-22 16:18:50 +0200

    Create metrics_optimizer.sh

---
+++ metrics_optimizer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Путь к файлу службы
+service_file="/lib/systemd/system/prometheus-node-exporter.service"
+
+# Новая строка ExecStart
+new_execstart='ExecStart=/usr/bin/prometheus-node-exporter $ARGS --collector.disable-defaults --collector.textfile --collector.stat --collector.uname --collector.meminfo --collector.cpu --collector.loadavg --collector.filesystem --collector.diskstats --collector.netstat --collector.sockstat --collector.netdev --collector.textfile.directory=/var/lib/prometheus/node-exporter'
+
+# Замена строки ExecStart на новую строку
+sed -i "/^ExecStart/c\\$new_execstart" "$service_file"
+
+# Перезагрузка конфигурации systemd и перезапуск службы
+systemctl daemon-reload
+systemctl restart prometheus-node-exporter
+
+echo "Строка ExecStart успешно обновлена в файле $service_file и служба перезапущена."
